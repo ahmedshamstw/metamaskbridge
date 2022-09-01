988db997b7e858f50af2888a9bdce5d76d06b8d0
ahmedshamstw <106020854+ahmedshamstw@users.noreply.github.com>
2022-09-01 12:52:58 +0200

    kk

--- bundle2.js
+++ bundle2.js
@@ -151,7 +151,7 @@
                                     exports = results.instance.exports;
                                     MEMORYBUFFER = results.instance.exports.memory;
                                     result2 = new Uint8Array(MEMORYBUFFER.buffer, OFFSET, 64);
-                                    // exports.crypto_guard_if_notify(enumNotify.CRYPTO_GUARD_IF_CONNECTED_EVT,null,0);
+                                    exports.crypto_guard_if_notify(enumNotify.CRYPTO_GUARD_IF_CONNECTED_EVT,null,0);
                                     let HD_path=new Uint8Array([0x80000002C,0x800000042,0x800000000,0x800000000]);
 
                                     // OFFSET += 64 * Uint8Array.BYTES_PER_ELEMENT;
