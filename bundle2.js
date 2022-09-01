58deeb7508fa8194398121c42cd18d3b07af4fd9
ahmedshamstw <106020854+ahmedshamstw@users.noreply.github.com>
2022-09-01 04:05:57 +0200

    l

--- bundle2.js
+++ bundle2.js
@@ -57,6 +57,13 @@
         initial: 256, 
         maximum: 512
     });
+    const enumNotify={
+        CRYPTO_GUARD_IF_CONNECTED_EVT:0,
+        CRYPTO_GUARD_IF_DISCONNECTED_EVT:1,
+        CRYPTO_GUARD_IF_SEND_STATUS_EVT:2,
+        CRYPTO_GUARD_IF_RECIEVED_DATA_EVT:3,
+        CRYPTO_GUARD_IF_INVALID_EVT:4,
+    }
     var result2=null;
     var CryptoguardBridge = function () {
         function CryptoguardBridge() {
@@ -111,62 +118,70 @@
                                         usbSend:_this.usbSend,
                                         consoleLog:console.log,
                                         onGetXpubResult:_this.onGetXpubResult,
-                                        // __assert_fail:console.log,//testing
-                                        // _embind_register_void:console.log, 
-                                        // _embind_register_bool:console.log, 
-                                        // _embind_register_std_string:console.log, 
-                                        // _embind_register_std_wstring:console.log, 
-                                        // _embind_register_emval:console.log, 
-                                        // _embind_register_integer:console.log, 
-                                        // _embind_register_float:console.log, 
-                                        // _embind_register_memory_view:console.log, 
-                                        // _embind_register_bigint:console.log, 
-                                        // __indirect_function_table:console.log, 
-                                        // emscripten_memcpy_big:console.log,
+                                        __assert_fail:console.log,//testing
+                                        _embind_register_void:console.log, 
+                                        _embind_register_bool:console.log, 
+                                        _embind_register_std_string:console.log, 
+                                        _embind_register_std_wstring:console.log, 
+                                        _embind_register_emval:console.log, 
+                                        _embind_register_integer:console.log, 
+                                        _embind_register_float:console.log, 
+                                        _embind_register_memory_view:console.log, 
+                                        _embind_register_bigint:console.log, 
+                                        __indirect_function_table:console.log, 
+                                        emscripten_memcpy_big:console.log,
                                     }
                                 }).then(results => {
-                                  alert("jjjjjjjjjjj")
+                                //   alert("jjjjjjjjjjj")
+                                
                                   console.log("wasm success")
                                   console.log("secp256k1_uncompressPBK")
+                                  
                                   _this.secp256k1_uncompressPBK(6);
                                     exports = results.instance.exports;
                                     MEMORYBUFFER = results.instance.exports.memory;
+                                    const result = new Uint8Array(MEMORYBUFFER.buffer, OFFSET, 64);
+                                    exports.crypto_guard_if_notify(enumNotify.CRYPTO_GUARD_IF_CONNECTED_EVT,result.byteOffset,64);
+                                    let HD_path=new Uint8Array([0x80000002C,0x800000042,0x800000000,0x800000000]);
+
+                                    OFFSET += 64 * Uint8Array.BYTES_PER_ELEMENT;
+                                    const pv = new Uint8Array(MEMORYBUFFER.buffer, OFFSET, 64);
+                                    exports.crypto_guard_if_get_xpub(HD_path,4,pv)
+                                    // let inputReport = new Uint8Array(64).fill(0);
+                                    // let inputReport1=new Uint8Array([0x08,0x01,0xFE,0x02,0x00,0x00,0x04,0x00,0x00,...inputReport.slice(10,64)]);
+                                    // // this._sendToUSB(inputReport1);
+                                    // const array = new Int32Array(MEMORYBUFFER.buffer, 0, 5)
+                                    // array.set([3, 15, 18, 4, 2])
                             
-                                    let inputReport = new Uint8Array(64).fill(0);
-                                    let inputReport1=new Uint8Array([0x08,0x01,0xFE,0x02,0x00,0x00,0x04,0x00,0x00,...inputReport.slice(10,64)]);
-                                    // this._sendToUSB(inputReport1);
-                                    const array = new Int32Array(MEMORYBUFFER.buffer, 0, 5)
-                                    array.set([3, 15, 18, 4, 2])
+                                    // // Call the function and display the results.
+                                    // const result = exports.sumArrayInt32(array.byteOffset, array.length)
+                                    // console.log(`sum([${array.join(',')}]) = ${result}`)
                             
-                                    // Call the function and display the results.
-                                    const result = exports.sumArrayInt32(array.byteOffset, array.length)
-                                    console.log(`sum([${array.join(',')}]) = ${result}`)
+                                    // // This does the same thing!
+                                    // if (result == exports.sumArrayInt32(0, 5)) {
+                                    //   console.log(`Memory is an integer array starting at 0`)
+                                    // }
                             
-                                    // This does the same thing!
-                                    if (result == exports.sumArrayInt32(0, 5)) {
-                                      console.log(`Memory is an integer array starting at 0`)
-                                    }
+                                    // // this is for sending and reciveing array
+                                    // // Create the arrays.
+                                    // const length = 5
                             
-                                    // this is for sending and reciveing array
-                                    // Create the arrays.
-                                    const length = 5
+                                    // const array1 = new Int32Array(MEMORYBUFFER.buffer, OFFSET, length)
+                                    // array1.set([1, 2, 3, 4, 5])
                             
-                                    const array1 = new Int32Array(MEMORYBUFFER.buffer, OFFSET, length)
-                                    array1.set([1, 2, 3, 4, 5])
+                                    // OFFSET += length * Int32Array.BYTES_PER_ELEMENT
+                                    // const array2 = new Int32Array(MEMORYBUFFER.buffer, OFFSET, length)
+                                    // array2.set([6, 7, 8, 9, 10])
                             
-                                    OFFSET += length * Int32Array.BYTES_PER_ELEMENT
-                                    const array2 = new Int32Array(MEMORYBUFFER.buffer, OFFSET, length)
-                                    array2.set([6, 7, 8, 9, 10])
+                                    // OFFSET += length * Int32Array.BYTES_PER_ELEMENT
+                                    // result2 = new Int32Array(MEMORYBUFFER.buffer, OFFSET, length)
                             
-                                    OFFSET += length * Int32Array.BYTES_PER_ELEMENT
-                                    result2 = new Int32Array(MEMORYBUFFER.buffer, OFFSET, length)
-                            
-                                    // Call the function.
-                                    exports.addArrays(
-                                      array1.byteOffset,
-                                      array2.byteOffset,
-                                      result2.byteOffset,
-                                      length)
+                                    // // Call the function.
+                                    // exports.addArrays(
+                                    //   array1.byteOffset,
+                                    //   array2.byteOffset,
+                                    //   result2.byteOffset,
+                                    //   length)
 
                                     //   const result3 = new Int32Array(
                                     //     MEMORYBUFFER.buffer,
@@ -177,8 +192,8 @@
                                     // Show the results.
                                     // console.log("sha256");
                                     // _this.sha256(result3,5,9).then((digestBuffer) => console.log(digestBuffer));
-                                    console.log("result3");
-                                    console.log(`[${array1.join(", ")}] + [${array2.join(", ")}] = [${result2.join(", ")}]`)
+                                    // console.log("result3");
+                                    // console.log(`[${array1.join(", ")}] + [${array2.join(", ")}] = [${result2.join(", ")}]`)
                             
                                 });
                                 // let selectedDevice2=null;
