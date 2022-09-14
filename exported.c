e5dfa798f97cd72bcc46a4224ad27701d5b9ca16
ahmedshamstw <106020854+ahmedshamstw@users.noreply.github.com>
2022-09-14 10:59:12 +0200

    fff

--- exported.c
+++ exported.c
@@ -15,7 +15,8 @@ EMSCRIPTEN_KEEPALIVE
 int itWorkshhh(int x,int y)
 {
     int* pstr_cntxt = malloc(10000);
-    consoleLog("testing");
+    char* asd="asd";
+    consoleLog(asd);
 	if(NULL != pstr_cntxt)return 9;
     return x+y;
 }
